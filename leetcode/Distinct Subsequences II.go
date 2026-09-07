9c838dbe79b63c991d4c2b2633fe419bf2712747
Soham Babrekar <145750956+Sohamsig@users.noreply.github.com>
2026-09-07 21:30:46 +0530

    Create  Distinct Subsequences II.go

---
+++ leetcode/Distinct Subsequences II.go
@@ -0,0 +1,15 @@
+optimized sol:-
+
+func distinctSubseqII(s string) int {
+    const mod int = 1e9 + 7
+    f := [26]int{}
+    ans := 0
+    for _, c := range s {
+        i := c - 'a'
+        add := (ans + 1 + mod - f[i]) % mod
+        ans = (ans + add) % mod
+        f[i] = (f[i] + add) % mod
+    }
+    return ans
+}
+
